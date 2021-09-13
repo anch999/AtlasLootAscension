094b22617d7f0a739fcd0f3d26b8c7a0d5657f56
Szyler <Szyler@Szyler.com>
2021-09-13 13:43:49 +0200

    Base options

--- AtlasLoot/Core/Options.lua
+++ AtlasLoot/Core/Options.lua
@@ -237,6 +237,7 @@ function AtlasLoot_CreateOptionsInfoTooltips()
       AtlasLoot_AddTooltip("AtlasLootOptionsFrame_FuBarShow", nil) -- AL["Show FuBar Plugin"]
       AtlasLoot_AddTooltip("AtlasLootOptionsFrame_FuBarHide", nil) -- AL["Hide FuBar Plugin"]
       AtlasLoot_AddTooltip("AtlasLoot_SelectLootBrowserStyle", nil) 
+      AtlasLoot_AddTooltip("AtlasLoot_SelectMythicPlussTier", nil) 
 end
 
 function AtlasLoot_OptionsOnShow()
@@ -248,6 +249,10 @@ function AtlasLoot_OptionsOnShow()
     UIDropDownMenu_Initialize(AtlasLoot_CraftingLink, AtlasLoot_CraftingLink_Initialize);
 	UIDropDownMenu_SetSelectedID(AtlasLoot_CraftingLink, AtlasLoot.db.profile.CraftingLink);
 	UIDropDownMenu_SetWidth(AtlasLoot_CraftingLink, 150);
+    AtlasLoot_SelectMythicPlussTier_Label:SetText(AL["Select Mythic Pluss Tier:"]);
+    UIDropDownMenu_Initialize(AtlasLoot_SelectMythicPlussTier, AtlasLoot_SelectMythicPlussTier_Initialize);
+	UIDropDownMenu_SetSelectedID(AtlasLoot_SelectMythicPlussTier, AtlasLoot.db.profile.MythicPlussTier);
+	UIDropDownMenu_SetWidth(AtlasLoot_SelectMythicPlussTier, 150);
 end
 
 function AtlasLoot_SelectLootBrowserStyle_Initialize()
@@ -292,8 +297,32 @@ end
 
 function AtlasLoot_CraftingLink_OnClick()
     local thisID = this:GetID();
-	UIDropDownMenu_SetSelectedID(AtlasLoot_CraftingLink, thisID);
+    UIDropDownMenu_SetSelectedID(AtlasLoot_CraftingLink, thisID);
     AtlasLoot.db.profile.CraftingLink = thisID;
+    if AtlasLootItemsFrame:IsVisible() and AtlasLootItemsFrame.refresh then
+        AtlasLoot_ShowItemsFrame(AtlasLootItemsFrame.refresh[1], AtlasLootItemsFrame.refresh[2], AtlasLootItemsFrame.refresh[3], AtlasLootItemsFrame.refresh[4]);
+    end
+    AtlasLoot_OptionsOnShow();
+end
+
+function AtlasLoot_SelectMythicPlussTier_Initialize()
+	local info;
+	info = {
+        text = AL["Mythic Tier 1"];
+        func = AtlasLoot_MythicPlussTier_OnClick;
+    };
+	UIDropDownMenu_AddButton(info);
+    info = {
+        text = AL["Mythic Tier 2"];
+        func = AtlasLoot_MythicPlussTier_OnClick;
+    };
+	UIDropDownMenu_AddButton(info);
+end
+
+function AtlasLoot_MythicPlussTier_OnClick()
+    local thisID = this:GetID();
+    UIDropDownMenu_SetSelectedID(AtlasLoot_SelectMythicPlussTier, thisID);
+    AtlasLoot.db.profile.MythicPlussTier = thisID;
     if AtlasLootItemsFrame:IsVisible() and AtlasLootItemsFrame.refresh then
         AtlasLoot_ShowItemsFrame(AtlasLootItemsFrame.refresh[1], AtlasLootItemsFrame.refresh[2], AtlasLootItemsFrame.refresh[3], AtlasLootItemsFrame.refresh[4]);
     end
