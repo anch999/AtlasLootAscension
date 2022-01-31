ff5a39c4a81ab7c12841b550d67841cb855f2138
merstrax <merstrax@gmail.com>
2022-01-31 00:49:53 -0500

    Added Item fixes to ItemDB

---
+++ AtlasLoot/Core/ItemIDsDatabaseFixes.lua
@@ -0,0 +1,22 @@
+--Items not added from generated file
+ItemIDsDatabase["Bindings of the Windseeker (Left)"] = {18563, 18563, 318563, 218563};
+ItemIDsDatabase["Bindings of the Windseeker (Right)"] = {18564, 18564, 318564, 218564};
+ItemIDsDatabase["Eye of Sulfuras"] = {17204, 17204, 317204, 217204};
+ItemIDsDatabase["Essence of the Firelord"] = {19017, 19017, 319017, 219017};
+ItemIDsDatabase["Medallion of Binding Shard"] = {11879, 11879, 311879, 211879}; 
+ItemIDsDatabase["Atiesh, Greatstaff of the Guardian (Mage)"] = {6022589, 22589, 100174, 222589};
+ItemIDsDatabase["Atiesh, Greatstaff of the Guardian (Druid)"] = {6022632, 22632, 22632, 222632};
+ItemIDsDatabase["Atiesh, Greatstaff of the Guardian (Priest)"] = {6022631, 22631, 22631, 222631};
+ItemIDsDatabase["Atiesh, Greatstaff of the Guardian (Warlock)"] = {6022630, 22630, 22630, 222630};
+
+ItemIDsDatabase["Bottled Nethergron Extract"] = {6053891, 2032902, 2232902, 2332902};
+ItemIDsDatabase["Mmrgrg glm Nuubs"] = {6044831, 44831, 344831, 244831};
+ItemIDsDatabase["Alembic of Infernal Power"] = {6027896, 27896, 27896, 427896};
+
+ItemIDsDatabase["The Darkener's Grasp"] = {6030007, 30007, 330007, 230007};
+ItemIDsDatabase["The Sun King's Talisman"] = {6030015, 30015, 330015, 230015};
+ItemIDsDatabase["Telonicus's Pendant of Mayhem"] = {6030017, 30017, 330017, 230017};
+ItemIDsDatabase["Lord Sanguinar's Claim"] = {6030018, 30018, 330018, 230018};
+ItemIDsDatabase["Warglaive of Azzinoth (Mainhand)"] = {6032837, 32837, 332837, 232837};
+ItemIDsDatabase["Warglaive of Azzinoth (Offhand)"] = {6032838, 32838, 332838, 232838};
+ItemIDsDatabase["Cobra-Lash Boots"] = {6033482, 30104, 330104, 230104};
