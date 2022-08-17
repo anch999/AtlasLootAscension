ca93a7992ff81e973e7de43f2aa890b5591aa529
merstrax <merstrax@gmail.com>
2022-08-17 11:14:33 -0400

    Search table no longer queries all items on show -This caused a refresh loop and could cause fun issues if you press search button a couple times -Search already queries the item before it can even be added

--- AtlasLoot/Core/AtlasLoot.lua
+++ AtlasLoot/Core/AtlasLoot.lua
@@ -770,7 +770,9 @@ function AtlasLoot:ShowItemsFrame(dataID, dataSource, tablenum)
 		AtlasLoot:HideNoUsableItems();
 	end
 
-	AtlasLoot:QueryLootPage();
+	if dataID ~= "SearchResult" then
+		AtlasLoot:QueryLootPage();
+	end
 end
 
 --[[
