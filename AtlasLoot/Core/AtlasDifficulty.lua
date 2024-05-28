46bc388bfe77a94e5728628b265a9cb2b92100ff
Anch <adrianpratt.ap@gmail.com>
2024-05-28 21:39:49 +1200

    Update AtlasDifficulty.lua

--- AtlasLoot/Core/AtlasDifficulty.lua
+++ AtlasLoot/Core/AtlasDifficulty.lua
@@ -15,6 +15,7 @@
 
     AtlasLoot.Difficultys["ClassicDungeonExt"] = {
         {"Bloodforged", 1},
+        {"Heroic Bloodforged", 100},
         {"Normal", 2},
         {"Heroic", 3},
         {"Mythic", 4},
@@ -39,6 +40,7 @@
 
     AtlasLoot.Difficultys["BCDungeon"] = {
         {"Bloodforged", 1},
+        {"Heroic Bloodforged", 100},
         {"Normal/Heroic", 2},
         {"Mythic", 4},
         {"Mythic 1", 5 },  {"Mythic 2", 6 },  {"Mythic 3", 7 },  {"Mythic 4", 8 },  {"Mythic 5", 9 },
@@ -64,6 +66,7 @@
         {"Normal/Heroic", 2},
         {"Mythic", 4},
         {"Bloodforged", 1},
+        {"Heroic Bloodforged", 100},
         {"Mythic 1", 5 },  {"Mythic 2", 6 },  {"Mythic 3", 7 },  {"Mythic 4", 8 },  {"Mythic 5", 9 },
         {"Mythic 6", 10 }, {"Mythic 7", 11 }, {"Mythic 8", 12 }, {"Mythic 9", 13 }, {"Mythic 10", 14 },
         {"Mythic 11", 15 }, {"Mythic 12", 16 }, {"Mythic 13", 17 }, {"Mythic 14", 18 }, {"Mythic 15", 19 },
