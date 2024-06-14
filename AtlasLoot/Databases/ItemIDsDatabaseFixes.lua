e262cd0a5a20a11610bd4da6f32990464c4c490b
Anch <adrianpratt.ap@gmail.com>
2024-06-14 23:47:38 +1200

    Update ItemIDsDatabaseFixes.lua

--- AtlasLoot/Databases/ItemIDsDatabaseFixes.lua
+++ AtlasLoot/Databases/ItemIDsDatabaseFixes.lua
@@ -7,6 +7,26 @@ ItemIDManuelCorrections[22489] = { nil, nil, 322489, 1322489, 222489 }
 ItemIDManuelCorrections[1507043] = { nil, nil, 1807043, 2807043, 1707043 }
 ItemIDManuelCorrections[1523064] = { nil, nil, 1823064, 2823064, 1723064 }
 
+ItemIDManuelCorrections[16900] = { nil, nil, 316921, 1316921, 216921 }
+
+
+ItemIDManuelCorrections[16939] = { nil, nil, 316939, 1316939, 216939 }
+ItemIDManuelCorrections[16914] = { nil, nil, 316914, 1316914, 216914 }
+ItemIDManuelCorrections[16955] = { nil, nil, 316955, 1316955, 216955 }
+ItemIDManuelCorrections[16921] = { nil, nil, 316921, 1316921, 216921 }
+ItemIDManuelCorrections[16908] = { nil, nil, 316908, 1316908, 216908 }
+ItemIDManuelCorrections[16947] = { nil, nil, 316947, 1316947, 216947 }
+ItemIDManuelCorrections[10962] = { nil, nil, 310962, 1310962, 210962 }
+ItemIDManuelCorrections[16929] = { nil, nil, 316929, 1316929, 216929 }
+ItemIDManuelCorrections[10904] = { nil, nil, 310904, 1310904, 210904 }
+ItemIDManuelCorrections[16963] = { nil, nil, 316963, 1316963, 216963 }
+ItemIDManuelCorrections[1516955] = { nil, nil, 1816955, 2816955, 1716955 }
+ItemIDManuelCorrections[1516921] = { nil, nil, 1816921, 2816921, 1716921 }
+ItemIDManuelCorrections[1516947] = { nil, nil, 1816947, 2816947, 1716947 }
+ItemIDManuelCorrections[1516963] = { nil, nil, 1816963, 2816963, 1716963 }
+ItemIDManuelCorrections[1516900] = { nil, nil, 1816900, 2816900, 1716900 }
+ItemIDManuelCorrections[1516892] = { nil, nil, 1816892, 2816892, 1716892 }
+
 
 --ZA Bears
 ItemIDManuelCorrections[1333809] = { nil, nil, 1433809, nil, 1233809 }
