bbf70f703a97b0458f8e1f0055649de336334ac7
Anch <adrianpratt.ap@gmail.com>
2024-11-21 23:39:27 +1300

    Update PatchNotes.lua

--- AtlasLoot/Core/PatchNotes.lua
+++ AtlasLoot/Core/PatchNotes.lua
@@ -1,5 +1,7 @@
 
 local patchNotes = {
+    {"Version 7.4.5", "21st November, 2024"},
+    "Added option to show an item glow on items in a vendor that are in your wishlists",
     {"Version 7.4.4", "15th November, 2024"},
     "Rebuilt the TBC PVP rewards module",
     {"Version 7.4.3", "11th November, 2024"},
