5df56599ef80f004ce00c7a9d79689a2a58377ca
Anch <adrianpratt.ap@gmail.com>
2024-11-29 02:12:34 +1300

    moved all the profession functions to there own lua

--- AtlasLoot/Core/ProfessionUtils.lua
+++ AtlasLoot/Core/ProfessionUtils.lua
@@ -15,6 +15,7 @@ local playerName = UnitName("player")
 local realmName = GetRealmName()
 local playerFaction = UnitFactionGroup("player")
 
+-- returns all the info about a recipe
 function AtlasLoot:GetRecipeData(recipeID, idType)
 	if not TRADESKILL_RECIPES then return end
 	for _,prof in pairs(TRADESKILL_RECIPES) do
@@ -58,15 +59,16 @@ function AtlasLoot:GetRecipeID(spellID)
 	 end
 end
 
+-- return true if recipe is known
 function AtlasLoot:IsRecipeKnown(ID, profile)
-	if not profile or not profile.professions then return end
+	if not ID or not profile or not profile.professions then return end
 	for _, prof in pairs(profile.professions) do
 		if prof.knownRecipes[ID] then return true end
 	end
 end
 
+ -- returns a list of characters with the recipe
 function AtlasLoot:GetKnownRecipes(spellID)
-    --returns a list of characters with the recipe
 	local text
     for key, profile in pairs(self.db.profiles) do
         if gsub(key,"-",""):match(gsub(realmName,"-","")) and not gsub(key,"-",""):match(gsub(playerName,"-","")) and self:IsRecipeKnown(spellID, profile) then
@@ -77,11 +79,14 @@ function AtlasLoot:GetKnownRecipes(spellID)
 	return text
 end
 
+-- checks to see if character knows a profession
 function AtlasLoot:IsProfessionKnown(skillID, profile)
 	if profile and profile.professions and profile.professions[skillID] then return true end
 end
 
+-- create a list of every character that dosnt know this recipe
 function AtlasLoot:IsRecipeUnknown(ID)
+	if not ID then return end
 	local recipeData = self:GetRecipeData(ID, "item")
 	if not recipeData or not recipeData.skillIndex then return end
 
@@ -153,6 +158,7 @@ local professionTable = {
 
 local craftingXpac = { ClassicCrafting = 1, BCCrafting = 2, WrathCrafting = 3 }
 
+-- Sets pins on the map for all unknown tradeskill recipes if tomtom is installed 
 function AtlasLoot:SetRecipeMapPins()
 	local xpac = GetAccountExpansionLevel()+1
 	self:LoadAllModules()
@@ -185,6 +191,7 @@ function AtlasLoot:SetRecipeMapPins()
 	end
 end
 
+-- returns table of all the recipe source information
 function AtlasLoot:GetRecipeSource(spellID)
 	if not spellID then return end
 	local cData = AtlasLoot_CraftingData
@@ -307,4 +314,92 @@ function AtlasLoot:GetRecipeSource(spellID)
 		end
 	end
 	return data
+end
+
+-- Creates list characters known recipes
+function AtlasLoot:PopulateProfessions()
+	self.db.profile.professions = self.db.profile.professions or {}
+	for _, skillID in pairs(PRIMARY_PROFESSIONS) do
+		local _, _, _, skillMaxRank = GetSkillInfo(skillID)
+		if skillMaxRank and skillMaxRank > 0 then
+			self.db.profile.professions[skillID] = self.db.profile.professions[skillID] or { knownRecipes = {} }
+		end
+	end
+	for _, skillID in pairs(SECONDARY_PROFESSIONS) do
+		local _, _, _, skillMaxRank = GetSkillInfo(skillID)
+		if skillMaxRank and skillMaxRank > 0 then
+			self.db.profile.professions[skillID] = self.db.profile.professions[skillID] or { knownRecipes = {} }
+		end
+	end
+	for prof, _ in pairs(self.db.profile.professions) do
+		if TRADESKILL_RECIPES[prof] then
+			for _,cat in pairs(TRADESKILL_RECIPES[prof]) do
+				for _,recipe in pairs(cat) do
+					if CA_IsSpellKnown(recipe.SpellEntry) then
+						self.db.profile.professions[prof].knownRecipes[recipe.SpellEntry] = true
+					end
+				end
+			end
+		end
+	end
+end
+
+-- Loads the tradeskill data .json
+function AtlasLoot:LoadTradeskillRecipes()
+	if TRADESKILL_RECIPES then return end
+		TRADESKILL_RECIPES = {}
+		TRADESKILL_CRAFTS = {}
+
+		local fmtSubClass = "ITEM_SUBCLASS_%d_%d"
+		local fmtTotem = "SPELL_TOTEM_%d"
+		local fmtObject = "SPELL_FOCUS_OBJECT_%d"
+
+		local content = C_ContentLoader:Load("TradeSkillRecipeData")
+
+		local function GetToolName(toolID)
+			return _G[format(fmtTotem, toolID)]
+		end
+
+		content:SetParser(function(_, data)
+			if not TRADESKILL_RECIPES[data.SkillIndex] then
+				TRADESKILL_RECIPES[data.SkillIndex] = {}
+			end
+
+			data.Category = _G[format(fmtSubClass, data.CreatedItemClass, data.CreatedItemSubClass)]
+
+			if not TRADESKILL_RECIPES[data.SkillIndex][data.Category] then
+				TRADESKILL_RECIPES[data.SkillIndex][data.Category] = {}
+			end
+
+			data.IsHighRisk = toboolean(data.IsHighRisk)
+
+			-- reformat reagents
+			data.Reagents = {}
+			local reagents = data.ReagentData:SplitToTable(",")
+			for _, reagentString in ipairs(reagents) do
+				local item, count = reagentString:match("(%d*):(%d*)")
+				item = tonumber(item)
+				count = tonumber(count)
+				if item and item ~= 0 and count and count ~= 0 then
+					tinsert(data.Reagents, {item, count})
+				end
+			end
+
+			if #data.Reagents > 0 then
+				data.ReagentData = nil
+
+				-- reformat tools (totems)
+				data.Tools = data.TotemCategories:SplitToTable(",", GetToolName)
+				data.TotemCategories = nil
+
+				data.SpellFocusObject = _G[format(fmtObject, data.SpellFocusObject)]
+
+				tinsert(TRADESKILL_RECIPES[data.SkillIndex][data.Category], data)
+				if data.CreatedItemEntry > 0 then
+					TRADESKILL_CRAFTS[data.CreatedItemEntry] = data
+				end
+			end
+		end)
+
+		content:Parse()
 end
